87fdd0d901f000e85b197535a088c6eac866518b
minkiapps <minkiapps@gmail.com>
2022-06-10 16:14:15 +0200

    Add initial code

---
+++ app/src/main/java/com/minkiapps/hmsdemo/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.minkiapps.hmsdemo
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.huawei.hms.maps.HuaweiMap
+import com.huawei.hms.maps.MapsInitializer
+import com.huawei.hms.maps.OnMapReadyCallback
+import com.huawei.hms.maps.SupportMapFragment
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        MapsInitializer.initialize(this)
+
+        setContentView(R.layout.activity_main)
+
+        if (!hasPermissions(this, *RUNTIME_PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, RUNTIME_PERMISSIONS, REQUEST_CODE)
+        }
+
+        val supportMapFragment = supportFragmentManager
+            .findFragmentById(R.id.mapfragment_mapfragmentdemo) as SupportMapFragment?
+        supportMapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: HuaweiMap) {
+        map.isMyLocationEnabled = true
+    }
+
+    private fun hasPermissions(context: Context, vararg permissions: String): Boolean {
+        for (permission in permissions) {
+            if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+
+    companion object {
+        private const val REQUEST_CODE = 10001
+        val RUNTIME_PERMISSIONS = arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+}
